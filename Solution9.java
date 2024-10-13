98c68097e8e904898b7205db9d2d7a7643c0f071
Worldsverre <worldsverre@qq.com>
2024-10-13 10:50:20 +0800

    Fix solution and add unit tests

--- Solution9.java
+++ Solution9.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -36,51 +38,69 @@ import java.util.List;
  * dislikes 中每一组都 不同
  *
  */
-class Solution9 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution9 {
 
     public boolean possibleBipartition(int n, int[][] dislikes) {
+        // 初始化并查集数组
         int[] fa = new int[n + 1];
         Arrays.fill(fa, -1);
+
+        // 初始化邻接表，记录不喜欢关系
         List<Integer>[] g = new List[n + 1];
-        for (int i = 0; i < n; ++i) {
-            g[i] = new ArrayList<Integer>();
+        for (int i = 1; i <= n; ++i) {
+            g[i] = new ArrayList<>();
         }
-        for (int[] p : dislikes)
+
+        // 填充不喜欢关系的邻接表
+        for (int[] p : dislikes) {
             g[p[0]].add(p[1]);
             g[p[1]].add(p[0]);
+        }
+
+        // 遍历每个人，进行二分检测
         for (int i = 1; i <= n; ++i) {
             for (int j = 0; j < g[i].size(); ++j) {
-                unit(g[i].get(0), g[i].get(j), fa);
-                if (isconnect(i, g[i].get(j), fa)) {
+                int firstPerson = g[i].get(0);
+                int dislikePerson = g[i].get(j);
+
+                // 先检查是否连通，若连通则返回 false
+                if (isconnect(i, dislikePerson, fa)) {
                     return false;
                 }
+
+                // 否则，将这些人归类到不同的组中
+                unit(firstPerson, dislikePerson, fa);
             }
         }
         return true;
     }
 
+    // 合并两个集合
     public void unit(int x, int y, int[] fa) {
         x = findFa(x, fa);
         y = findFa(y, fa);
-        if (x == y) {
-            return ;
+        if (x != y) {
+            if (fa[x] <= fa[y]) {
+                fa[x] += fa[y];
+                fa[y] = x;
+            } else {
+                fa[y] += fa[x];
+                fa[x] = y;
+            }
         }
-        if (fa[x] <= fa[y]) {
-            int temp = x;
-            x = y;
-            y = temp;
-        }
-        fa[x] += fa[y];
-        fa[y] = x;
     }
 
+    // 判断两个节点是否在同一个集合
     public boolean isconnect(int x, int y, int[] fa) {
-        x = findFa(x, fa);
-        y = findFa(y, fa);
-        return x == y;
+        return findFa(x, fa) == findFa(y, fa);
     }
 
+    // 查找父节点并进行路径压缩
     public int findFa(int x, int[] fa) {
-        return fa[x] > 0 ? x : (fa[x] = findFa(fa[x], fa));
+        return fa[x] < 0 ? x : (fa[x] = findFa(fa[x], fa));
     }
 }
